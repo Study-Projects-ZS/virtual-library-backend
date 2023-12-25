c3402327c420b0e2a7c790e3647f6c0bdda63021
als-samara <samaraalmeida379@gmail.com>
2023-12-25 15:26:24 -0300

    first commit

---
+++ src/main/java/com/studygroupzs/virtuallibrary/VirtualLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.studygroupzs.virtuallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studygroupzs/virtuallibrary/VirtualLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studygroupzs.virtuallibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
