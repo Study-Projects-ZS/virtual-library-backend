c7844b143e617d5e9d7b14a32acffc95bfe330d9
als-samara <samaraalmeida379@gmail.com>
2023-12-26 21:13:45 -0300

    Book Controller implementation

--- src/main/java/com/studygroupzs/virtuallibrary/controller/BookController.java
+++ src/main/java/com/studygroupzs/virtuallibrary/controller/BookController.java
@@ -1,17 +1,21 @@
 package com.studygroupzs.virtuallibrary.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.studygroupzs.virtuallibrary.model.Book;
 import com.studygroupzs.virtuallibrary.repository.BookRepository;
@@ -48,10 +52,25 @@ public class BookController {
 		return ResponseEntity.ok(bookRepository.findAllByTitleContainingIgnoreCase(title));
 	}
 	
-	@PostMapping("/books")
+	@PostMapping("/create")
 	public ResponseEntity<Book> registerBook(@Valid @RequestBody Book book){
 		return ResponseEntity.status(HttpStatus.CREATED) // ResponseEntity.created().body...
 				.body(bookRepository.save(book));
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Book> updateBook(@RequestBody @Valid Book book){
+		return ResponseEntity.status(HttpStatus.OK)
+		.body(bookRepository.save(book));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteBook(@PathVariable Long id) {
+		Optional<Book> book = bookRepository.findById(id);
+				if(book.isEmpty())
+					throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+				
+				bookRepository.deleteById(id);
 	}
 	
 }
